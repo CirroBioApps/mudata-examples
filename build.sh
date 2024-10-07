e2734df481a8a91abf353c8793b0b373a5903a75
Sam Minot <sminot@gmail.com>
2024-10-07 11:07:47 -0700

    Add stub for building static assets

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+# Stub for building static assets used for particular pages
+
+# # Set up a virtual environment
+# [ ! -d .venv ] && python3 -m venv .venv
+# source .venv/bin/activate
+
+# # Install requirements
+# python3 -m pip install -r requirements.txt
+
+# find hugo/content/post -name build.py | xargs python3
